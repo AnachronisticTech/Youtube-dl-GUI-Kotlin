5e2cad6b616220776f995b08013289a041127335
Daniel <danielmarriner@anachronistic-tech.co.uk>
2020-04-14 00:35:06 +0100

    Downloading now works for arbitrary directories

--- src/libuiMain/kotlin/com.anachronistic.daniel.youtube-dl-gui/main.kt
+++ src/libuiMain/kotlin/com.anachronistic.daniel.youtube-dl-gui/main.kt
@@ -1,6 +1,7 @@
 import libui.ktx.*
 import libui.ktx.draw.*
-import platform.posix.open
+import platform.posix.*
+import kotlinx.cinterop.*
 
 fun main() = appWindow(
     title = "Youtube-DL GUI",
@@ -31,7 +32,7 @@ fun main() = appWindow(
                 }
                 button("Update") {
                     action {
-                        platform.posix.system("youtube-dl -U -q")
+                        run("\"%cd%\"\\youtube-dl -U -q")
                     }
                 }
                 textfield {
@@ -39,13 +40,25 @@ fun main() = appWindow(
                 }.disable()
                 button("Download") {
                     action {
-                        var command = "${dlLocation.take(2)} && cd ${dlLocation}"
-                        var links = scroll.value.split("\n") as MutableList<String>
-                        links.removeAll { it == "" }
-                        for (link in links) {
-                            command += " && youtube-dl ${link} -x --audio-format mp3"
+                        if (scroll.value != "") {
+                            if (!scroll.value.contains("\n")) { scroll.append("\n") }
+
+                            val copyYDL = "copy \"%cd%\"\\youtube-dl.exe ${dlLocation}\\youtube-dl.exe"
+                            val copyFFM = " && copy \"%cd%\"\\ffmpeg.exe ${dlLocation}\\ffmpeg.exe"
+                            val changeDrv = " && ${dlLocation.take(2)}"
+                            val changeDir = " && cd ${dlLocation}"
+                            val delYDL = " && del ${dlLocation}\\youtube-dl.exe"
+                            val delFFM = " && del ${dlLocation}\\ffmpeg.exe"
+
+                            var command = copyYDL + copyFFM + changeDrv + changeDir
+                            var links = scroll.value.split("\n") as MutableList<String>
+                            links.removeAll { it == "" }
+                            for (link in links) {
+                                command += " && \"%cd%\"\\youtube-dl.exe ${link} ${if (true) "-x --audio-format mp3" else ""} --ffmpeg-location \"%cd%\"\\ffmpeg.exe"
+                            }
+                            command += delYDL + delFFM
+                            run(command)
                         }
-                        platform.posix.system(command)
                     }
                 }
             }
@@ -58,3 +71,5 @@ fun notYetImplemented() = MsgBoxError(
     text = "This feature does not yet exist.",
     details = "Please be patient."
 )
+
+fun run(command: String) = platform.posix.system(command)
