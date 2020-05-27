a70d8e03099fc7f9b1885bfe31c06c41908aed27
Daniel <danielmarriner@anachronistic-tech.co.uk>
2020-05-27 17:49:05 +0100

    Removed unused "build" shell script

--- buildApp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp build/bin/libui/releaseExecutable/Youtube-DL-GUI.kexe build/bin/libui/releaseExecutable/Youtube-DL-GUI.app/Contents/MacOS/Youtube-DL-GUI.kexe
-open build/bin/libui/releaseExecutable/Youtube-DL-GUI.app
