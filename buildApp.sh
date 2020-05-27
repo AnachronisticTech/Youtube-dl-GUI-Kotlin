acb84d9ee7119232e249eb03be330b1a027c0325
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2020-05-27 13:54:25 +0100

    Added script to build macOS .app

---
+++ buildApp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp build/bin/libui/releaseExecutable/Youtube-DL-GUI.kexe build/bin/libui/releaseExecutable/Youtube-DL-GUI.app/Contents/MacOS/Youtube-DL-GUI.kexe
+open build/bin/libui/releaseExecutable/Youtube-DL-GUI.app
